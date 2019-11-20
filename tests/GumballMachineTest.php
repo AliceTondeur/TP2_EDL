98ed12daf4a68325015c96d70a0a6fbda5c85862
AliceTondeur <44616570+AliceTondeur@users.noreply.github.com>
2019-11-20 12:06:12 +0100

    Update GumballMachineTest.php

--- tests/GumballMachineTest.php
+++ tests/GumballMachineTest.php
@@ -77,7 +77,7 @@ class GumballMachineTest extends PHPUnit_Framework_TestCase
     public function testInsertC()
     {
         #$this->gumballMachineInstance->DropData("cours");
-        $this->assertEquals(true,$this->gumballMachineInstance->InsertC($this->gumballMachineInstance->getDB(),$this->intitule1,$this->duree1,$this->gumballMachineInstance->GetIdP($this->nom1,$this->prenom1));
+        $this->assertEquals(true,$this->gumballMachineInstance->InsertC($this->gumballMachineInstance->getDB(),$this->intitule1,$this->duree1,$this->gumballMachineInstance->GetIdP($this->nom1,$this->prenom1)));
         $max__id1=$this->gumballMachineInstance->GetLastIDC();
         $this->assertEquals(true,$this->gumballMachineInstance->InsertC($this->gumballMachineInstance->getDB(),$this->intitule2,$this->duree2,$this->gumballMachineInstance->GetIdP($this->nom2,$this->prenom2)));
         $this->assertEquals(true,$this->gumballMachineInstance->InsertC($this->gumballMachineInstance->getDB(),$this->intitule3,$this->duree3,$this->gumballMachineInstance->GetIdP($this->nom3,$this->prenom3)));
