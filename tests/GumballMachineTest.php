e79dfc744de903a524131fb609f09b9103256ed7
AliceTondeur <44616570+AliceTondeur@users.noreply.github.com>
2019-11-20 09:42:12 +0100

    Update GumballMachineTest.php

--- tests/GumballMachineTest.php
+++ tests/GumballMachineTest.php
@@ -6,20 +6,20 @@ class GumballMachineTest extends PHPUnit_Framework_TestCase
 {
     public $gumballMachineInstance;
     //prof
-    private $nom="XXX1"; // a changer
-    private $prenom="YYY1"; // a changer
-    private $date_naissance="29-09-1980"; // a changer
-    private $lieu_naissance="ZZZ1"; // a changer
+    private $nom1="XXX1"; // a changer
+    private $prenom1="YYY1"; // a changer
+    private $date_naissance1="29-09-1980"; // a changer
+    private $lieu_naissance1="ZZZ1"; // a changer
     
-    private $nom="XXX2"; // a changer
-    private $prenom="YYY2"; // a changer
-    private $date_naissance="30-10-1981"; // a changer
-    private $lieu_naissance="ZZZ2"; // a changer
+    private $nom2="XXX2"; // a changer
+    private $prenom2="YYY2"; // a changer
+    private $date_naissance2="30-10-1981"; // a changer
+    private $lieu_naissance2="ZZZ2"; // a changer
     
-    private $nom="XXX3"; // a changer
-    private $prenom="YYY3"; // a changer
-    private $date_naissance="31-12-1982"; // a changer
-    private $lieu_naissance="ZZZ3"; // a changer
+    private $nom3="XXX3"; // a changer
+    private $prenom3="YYY3"; // a changer
+    private $date_naissance3="31-12-1982"; // a changer
+    private $lieu_naissance3="ZZZ3"; // a changer
     // cours
     private $intitule="IA"; //a remplir
     private $duree="12";    //a remplir
@@ -37,9 +37,12 @@ class GumballMachineTest extends PHPUnit_Framework_TestCase
     public function testInsertP()
     {
         $max__id1=$this->gumballMachineInstance->GetLastIDP();
-        $this->assertEquals(true,$this->gumballMachineInstance->InsertP($this->gumballMachineInstance->getDB(),$this->nom,$this->prenom,$this->date_naissance,$this->lieu_naissance));
+        $this->assertEquals(true,$this->gumballMachineInstance->InsertP($this->gumballMachineInstance->getDB(),$this->nom1,$this->prenom1,$this->date_naissance1,$this->lieu_naissance1));
+        $this->assertEquals(true,$this->gumballMachineInstance->InsertP($this->gumballMachineInstance->getDB(),$this->nom2,$this->prenom2,$this->date_naissance2,$this->lieu_naissance2));
+        $this->assertEquals(true,$this->gumballMachineInstance->InsertP($this->gumballMachineInstance->getDB(),$this->nom3,$this->prenom3,$this->date_naissance3,$this->lieu_naissance3));
         $max__id2=$this->gumballMachineInstance->GetLastIDP();
-        $this->assertEquals($max__id1+1,$max__id2);
+        $this->assertEquals($max__id1+3,$max__id2);
+        
     }
     public function testAffichageProfAPI()
     {
