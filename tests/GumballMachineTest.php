34a8dd63a4bd042077f27c09ab748b60a28a6f97
AliceTondeur <44616570+AliceTondeur@users.noreply.github.com>
2019-11-20 10:47:53 +0100

    Update GumballMachineTest.php

--- tests/GumballMachineTest.php
+++ tests/GumballMachineTest.php
@@ -44,7 +44,7 @@ class GumballMachineTest extends PHPUnit_Framework_TestCase
     public function setUp()
     {
         $this->gumballMachineInstance = new GumballMachine();
-        $this->gumballMachineInstance->DropData(this->getDB());
+        $this->gumballMachineInstance->DropData();
     }
     
     public function testAffichageProfAVI()
