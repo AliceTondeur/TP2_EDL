52fceab49fedc1c5bcb4bddcdf05d8b31e84f9c6
AliceTondeur <44616570+AliceTondeur@users.noreply.github.com>
2019-11-27 09:21:08 +0100

    Update GumballMachine.php

--- GumballMachine.php
+++ GumballMachine.php
@@ -204,4 +204,19 @@ public function AffichageProf($etat)
 		return false;
 	    }
 	}
+	public function DeleteC($id)
+	{
+	    try
+	    {
+		$this->bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$sql = "DELETE FROM cours WHERE id = '$id'";
+		$this->bdd->exec($sql);
+		return true;
+	    }
+	    catch(PDOException $e)
+	    {
+	        echo $sql . "<br>" . $e->getMessage();
+		return false;
+	    }
+	}
 }
